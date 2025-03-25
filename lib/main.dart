0d562581c0297faec8b146964fe4c0e8edc3b112
Leozin <yazucrubert@gmail.com>
2025-03-24 21:42:09 -0300

    estilização do cormen

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,7 @@ void main() {
 class DataStructureApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Stack Simulator',
-      home: StackVisualizer(),
-    );
+    return MaterialApp(title: 'Stack Simulator', home: StackVisualizer());
   }
 }
 
@@ -32,24 +29,39 @@ class _StackVisualizerState extends State<StackVisualizer> {
         children: [
           Expanded(
             child: Center(
-              child: Column(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: _stack.elements.reversed
-                    .map((e) => Container(
-                          margin: EdgeInsets.symmetric(vertical: 5),
-                          width: 100,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Colors.blue,
-                            borderRadius: BorderRadius.circular(10),
+                children:
+                    _stack.elements.reversed
+                        .map(
+                          (e) => Container(
+                            margin: EdgeInsets.symmetric(
+                              vertical: 5,
+                              horizontal: 0,
+                            ),
+                            width: 100,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.zero,
+                              border: Border(
+                                top: BorderSide(color: Colors.black),
+                                left: BorderSide(color: Colors.black),
+                                right: BorderSide(color: Colors.black),
+                                bottom: BorderSide(color: Colors.black),
+                              ),
+                            ),
+                            alignment: Alignment.center,
+                            child: Text(
+                              '$e',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            ),
                           ),
-                          alignment: Alignment.center,
-                          child: Text(
-                            '$e',
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                        ))
-                    .toList(),
+                        )
+                        .toList(),
               ),
             ),
           ),
